d1eb71b4c3afef47b07965b030a6e5ac3a8c9167
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-02-13 22:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+  document.getElementById("btn").addEventListener("click", function () {
+        var selectElement = document.getElementById("colorSelect");
+        var selectedIndex = selectElement.selectedIndex;
+
+        if (selectedIndex !== -1) {
+            selectElement.remove(selectedIndex);
+        }
+    });
